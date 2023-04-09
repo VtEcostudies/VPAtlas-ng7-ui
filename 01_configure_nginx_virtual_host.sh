fcd194b6308bea32315e548e9e5d0a3d41dc0419
jloomisVCE <jloomis@vtecostudies.org>
2023-04-09 21:17:27 +0000

    bail on ss logic. just make it operator managed.

--- 01_configure_nginx_virtual_host.sh
+++ 01_configure_nginx_virtual_host.sh
@@ -3,11 +3,7 @@ sudo rm /etc/nginx/sites-enabled/default
 # copy virtual host config files to /etc/nginx/sites-available
 sudo cp ./nginx_vpatlas_dev.conf /etc/nginx/sites-available/dev.vpatlas
 sudo cp ./nginx_vpatlas.conf /etc/nginx/sites-available/vpatlas
-# enable vpatlas nginx site
-host = $(hostname)
-if ["$host" = "dev.vpatlas.org"]; then
-    sudo ln -s /etc/nginx/sites-available/dev.vpatlas /etc/nginx/sites-enabled/dev.vpatlas
-fi
-if ["$host" = "vpatlas.org"]; then
-    sudo ln -s /etc/nginx/sites-available/vpatlas /etc/nginx/sites-enabled/vpatlas
-fi
+# enable the appropriate vpatlas nginx site
+sudo ln -s /etc/nginx/sites-available/dev.vpatlas /etc/nginx/sites-enabled/dev.vpatlas
+#sudo ln -s /etc/nginx/sites-available/vpatlas /etc/nginx/sites-enabled/vpatlas
+sudo systemctl restart nginx
