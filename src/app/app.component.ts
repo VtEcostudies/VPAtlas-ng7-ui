8bc431cc12231e5025901e97358ca1233d1c4e58
jloomisVCE <jloomis@vtecostudies.org>
2023-06-05 09:28:26 -0400

    new download method. downloads for login users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { AuthenticationService } from './_services';
 import { User, Auth } from './_models';
 import { environment } from '@environments/environment';
 import { UxValuesService } from '@app/_global';
-
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
     currentUserSubscription: Subscription;
@@ -54,17 +53,26 @@ export class AppComponent {
       this.router.navigate([`/user/profile/view/${this.currentUser.user.id}`]);
     }
 
-    download(dataType='mapped', fileType='csv') {
+    download(fileType='csv', dataType=null) {
+      if (!dataType) {
+        let type = this.uxValuesService.type;
+        if ('visi'==type) {dataType = 'visit';} else
+        if ('moni'==type) {dataType = 'survey';} else
+        {dataType='mapped';} //if ('all'==type || 'mine'==type)
+      }
       let townName = this.uxValuesService.filterParams.town.townName; townName = townName ? (townName != 'All' ? townName : '') : '';
       let userName = this.uxValuesService.filterParams.userName; userName = userName ? (userName != 'Unknown' ? userName : '') : '';
       let poolId = this.uxValuesService.filterParams.poolId; poolId = poolId ? poolId : '';
-      this.downloadParamsText = '';
+      this.downloadParamsText = 'download=1';
       this.downloadParamsText += townName ? `&townName=${townName}` : '';
       this.downloadParamsText += userName ? `&username=${userName}` : '';
       this.downloadParamsText += poolId ? `&mappedPoolId=${poolId}` : '';
       if ('pools' == dataType) {this.downloadParamsText = '';} //this query can't easily handle query params b/c it queries many tables.
 
-      let url = `${this.apiUrl}/${dataType}/${fileType}?download=1${this.downloadParamsText}`;
+      if ('shapefile' == fileType) { //authUser used to create filesystem download by user to handle simultaneity
+        this.downloadParamsText += `&authUser=${this.currentUser.user.username}`;
+      }
+      let url = `${this.apiUrl}/${dataType}/${fileType}?${this.downloadParamsText}`;
       window.open(url);
     }
 
